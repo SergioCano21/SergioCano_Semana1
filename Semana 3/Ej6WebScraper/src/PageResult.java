624a17c491d94a22429af4f1b2f8e90e35338d91
SergioCano21 <chimo.c.g@hotmail.com>
2026-05-07 13:14:57 -0600

    Add ej 6 week 3

---
+++ Semana 3/Ej6WebScraper/src/PageResult.java
@@ -0,0 +1,6 @@
+public record PageResult(String url, int statusCode, String title, long responseTimeMs) {
+  @Override
+  public String toString() {
+    return String.format("[%d] %s (%dms)", statusCode, title, responseTimeMs);
+  }
+}
